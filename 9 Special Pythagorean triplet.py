a03480ae322dc8aa52dbb838b0a1ef636d30cb5e
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-15 11:51:31 +0100

    extract constant from formula

--- 9 Special Pythagorean triplet.py
+++ 9 Special Pythagorean triplet.py
@@ -10,8 +10,10 @@ from math import sqrt, ceil
 #  b = (1000 * (a - 500)) / (a - 1000)
 #  c = sqrt(a ** 2 + b ** 2)
 
-for a in range(1, ceil(500 * (2 - sqrt(2)))):
-    b = int((1000 * (a - 500)) / (a - 1000))
+n = 1000
+
+for a in range(1, ceil((n / 2) * (2 - sqrt(2)))):
+    b = int((n * (a - n / 2)) / (a - n))
     c = int(sqrt(a ** 2 + b ** 2))
-    if a + b + c == 1000 and a ** 2 + b ** 2 == c ** 2:
+    if a + b + c == n and a ** 2 + b ** 2 == c ** 2:
         print(a * b * c)
