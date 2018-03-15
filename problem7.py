dc53302698e2c69b929ebf4e4c43c4519087d5d8
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-15 10:24:10 +0100

    improve problem 7

--- problem7.py
+++ problem7.py
@@ -1,10 +1,21 @@
+import math
+
 i, n = 2, 10001
 primes = []
-while len(primes) < n:    
-    while any([i % prime == 0 for prime in primes]):
-        i += 1
 
-    primes.append(i)
+def is_prime(n):
+    if n % 2 == 0: return n == 2
+
+    root = int(math.sqrt(n))
+    for d in range(3, root + 1):
+        if n % d == 0: return False
+
+    return True
+
+while len(primes) < n:
+    if is_prime(i):
+        primes.append(i)
+
     i += 1
 
 print(primes[-1])
