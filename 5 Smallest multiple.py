a3e50bc6de41c618105891f63816958a1829a5ce
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-15 11:02:42 +0100

    add problem 8

--- 5 Smallest multiple.py
+++ 5 Smallest multiple.py
@@ -1,7 +1,7 @@
 from functools import reduce
 
 def mul(array):
-    return reduce(lambda x, y: x * y, primes)
+    return reduce(lambda x, y: x * y, array)
 
 def missing_prime(i, primes):
     for prime in primes:
@@ -13,7 +13,7 @@ primes = []
 while i < n:
     primes.append(i)
     i += 1
-    
+
     while any([i % prime == 0 for prime in primes if not prime == 1]):
         if not mul(primes) % i == 0:
             primes.append(missing_prime(i, primes))
