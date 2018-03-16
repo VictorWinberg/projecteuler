80c3114700bdfa5a550a3a6ddb9080ae1e44e813
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-17 00:22:11 +0100

    add automation

--- purge_cache.sh
+++ purge_cache.sh
@@ -1,1 +1,11 @@
+# Get badge
+curl -I https://projecteuler.net/profile/VictorWinberg.png
+
+# Purge cache
 curl -X PURGE https://camo.githubusercontent.com/a08f41ce0c38352850fda8019ff7e0c916400c03/68747470733a2f2f70726f6a65637465756c65722e6e65742f70726f66696c652f566963746f7257696e626572672e706e673f
+
+# Get cache
+curl -I https://camo.githubusercontent.com/a08f41ce0c38352850fda8019ff7e0c916400c03/68747470733a2f2f70726f6a65637465756c65722e6e65742f70726f66696c652f566963746f7257696e626572672e706e673f
+
+# # Generate markdown
+python3 generate_markdown.py
