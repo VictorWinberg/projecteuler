bb71a2d33e3e2e37ba38d0fa15363695f31af976
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-17 00:23:03 +0100

    rename update

--- update.sh
+++ update.sh
@@ -7,5 +7,5 @@ curl -X PURGE https://camo.githubusercontent.com/a08f41ce0c38352850fda8019ff7e0c
 # Get cache
 curl -I https://camo.githubusercontent.com/a08f41ce0c38352850fda8019ff7e0c916400c03/68747470733a2f2f70726f6a65637465756c65722e6e65742f70726f66696c652f566963746f7257696e626572672e706e673f
 
-# # Generate markdown
+# Generate markdown
 python3 generate_markdown.py
