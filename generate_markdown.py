98717dbdba686eb78a5ab2d099d79aef50958c41
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-29 12:16:25 +0200

    add problem 11 and update generate markdown

--- generate_markdown.py
+++ generate_markdown.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from sys import stdout
 from time import time
 from math import log10, floor
 
@@ -15,23 +16,52 @@ def humanize(seconds):
     else:
         return str(int(round_sig(seconds))) + " s"
 
-title = "# Problem solutions\n"
-TOC = ["\n### Table of Contents\n"]
-content = ["\n## Contents"]
+def progress(fname, bar_length=50):
+    if fname:
+        spaces = ' ' * (bar_length - len(fname))
+        stdout.write('\rCurrent: {}'.format(fname + spaces))
+    else:
+        spaces = ' ' * bar_length
+        stdout.write('\rDone!{}\n'.format(spaces))
+    stdout.flush()
 
-folder = "problems/"
+if __name__ == '__main__':
+    print('''       _____                           _   _
+      / ____|                         | | (_)
+     | |  __  ___ _ __   ___ _ __ __ _| |_ _ _ __   __ _
+     | | |_ |/ _ \ '_ \ / _ \ '__/ _` | __| | '_ \ / _` |
+     | |__| |  __/ | | |  __/ | | (_| | |_| | | | | (_| |
+      \_____|\___|_| |_|\___|_| _\__,_|\__|_|_| |_|\__, |
+     |  \/  |          | |     | |                  __/ |
+     | \  / | __ _ _ __| | ____| | _____      ___ _|___/
+     | |\/| |/ _` | '__| |/ / _` |/ _ \ \ /\ / / '_ \\
+     | |  | | (_| | |  |   < (_| | (_) \ V  V /| | | |  _ _ _
+     |_|  |_|\__,_|_|  |_|\_\__,_|\___/ \_/\_/ |_| |_| (_|_|_)
+    ''')
+    title = "# Problem solutions\n"
+    TOC = ["\n### Table of Contents\n"]
+    content = ["\n## Contents"]
 
-for fname in sorted(os.listdir(folder)):
-    content.append("\n### {}".format(fname[:-3]))
-    TOC.append("{}. [{}](#{})".format(len(TOC), fname[4:-3], fname[:-3].replace(" ", "-").lower()))
-    with open(folder + fname) as f:
-        content.append("```python")
-        content.append(f.read())
-        content.append("```")
+    folder = "problems/"
+    fnames = sorted(os.listdir(folder))
+    for fname in fnames:
+        progress(fname)
 
-    start = time()
-    a = subprocess.run(['python3', folder + fname], stdout=subprocess.PIPE).stdout.decode('utf-8').strip()
-    content.append("Time: `~ {}`.".format(humanize(time() - start)))
+        content.append("\n### {}".format(fname[:-3]))
+        TOC.append("{}. [{}](#{})".format(len(TOC), fname[4:-3], fname[:-3].replace(" ", "-").lower()))
+        with open(folder + fname) as f:
+            content.append("```python")
+            content.append(f.read())
+            content.append("```")
 
-with open("solutions.md", 'w') as f:
-    f.write(title + '\n'.join(TOC + content))
+        end = float("inf")
+        for i in range(5):
+            start = time()
+            subprocess.run(['python3', folder + fname], stdout=subprocess.PIPE)
+            end = min(end, time() - start)
+        content.append("Time: `~ {}`.".format(humanize(end)))
+
+    with open("solutions.md", 'w') as f:
+        f.write(title + '\n'.join(TOC + content))
+
+    progress(None)
